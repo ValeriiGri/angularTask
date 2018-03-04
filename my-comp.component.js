f15c78ba05af5fd426bbe5a4a29ec495860a7970
ValeriiGri <valeragg65@gmail.com>
2018-03-04 11:32:24 +0300

    modal done

--- my-comp.component.js
+++ my-comp.component.js
@@ -5,19 +5,45 @@ angular.
       	controller: function MyController(){
       		var self = this;
 
-      		self.openWin = function openWin(){
-      			let div = document.querySelector('.second');
-      			div.classList.remove('invisible');
+      		// self.openWin = function openWin(){
+      		// 	let div = document.querySelector('.second');
+      		// 	div.classList.remove('invisible');
 
-      		};
+      		// };
+
+      		self.showCover = function showCover() {
+      			let coverDiv = document.createElement('div');
+      			coverDiv.classList.add('cover-div');
+      			document.body.appendChild(coverDiv);
+    		};
+
+    		self.hideCover = function hideCover() {
+		    	document.body.removeChild(document.querySelector('.cover-div'));
+		    };
+
+		    self.showPrompt = function showPrompt() {
+      			self.showCover();
+      			let input = document.querySelector('.modal-form>input');
+      			let container = document.querySelector('.modal-form-container');
+
+				function complete(value) {
+					hideCover();
+					container.style.display = 'none';
+				}
+
+		      container.style.display = 'block';
+		      input.focus();
+		    };
 
       		self.names = [
 
-							{id: 1, name: 'Света'},
+							{id: 1, name: 'Екатерина'},
+							{id: 2, name: 'Мария'},
+							{id: 3, name: 'Светлана'},
+							{id: 3, name: 'Михаил'},
+							{id: 3, name: 'Константин'},
+							{id: 3, name: 'Альберт'},
 
-							{id: 2, name: 'Маша'},
-
-							{id: 3, name: 'Лена'}
 
 						];
 
