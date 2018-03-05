c88e43434146bb835ec11782901252314b5cb8ed
ValeriiGri <valeragg65@gmail.com>
2018-03-05 23:11:20 +0300

    new version

--- my-comp.component.js
+++ my-comp.component.js
@@ -2,37 +2,18 @@ angular.
 	module('myApp').
 	component('myComp', {
 		templateUrl:'my-comp.template.html',
-      	controller: function MyController(){
+      	controller: function MyController($scope){
 
-      		this.coverDiv = document.createElement('div');
-      		this.container = document.querySelector('.modal-form-container');
-
-      		this.showCover = function showCover() {
-      			this.coverDiv.classList.add('cover-div');
-      			document.body.append(this.coverDiv);
-    		};
+      		$scope.hide = true;
 
 		    this.showModal = function showModal() {
-      			this.showCover();
-
-      			let input = document.querySelector('.modal-form>input');
-
-		      	this.container.style.display = 'block';
-		      	input.focus();
+		    	$scope.hide = false;
 		    };
 
-		    this.selectItem = function selectItem(){
-		    	let parentInput = document.querySelector('input[name=parentInput]');
-		    	let tbody = document.querySelector('tbody');
-		    	let self = this;
-
-		    	tbody.addEventListener('click', function(event){
-		    		let target = event.target;
-
-		    		parentInput.value = target.textContent;
-		    		self.container.style.display = 'none';
-		    		document.body.removeChild(self.coverDiv);
-		    	});
+		    this.selectItem = function selectItem(name){
+		    	$scope.nameId = name.id;
+		    	$scope.nameSelected = name.name;
+		    	$scope.hide = true;
 		    };
 
       		this.names = [
@@ -42,7 +23,7 @@ angular.
 							{id: 3, name: 'Светлана'},
 							{id: 4, name: 'Михаил'},
 							{id: 5, name: 'Константин'},
-							{id: 6, name: 'Альберт'},
+							{id: 6, name: 'Альберт'}
 
 
 						];
